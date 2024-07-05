ef3f662b06ae824352e76b454a1c562e459a06b8
rabbitglauser <samuel.glauser@gmail.com>
2024-07-05 09:21:45 +0200

    Add initial project setup for weather forecasting
    This commit introduces the basic structure for the future weather forecasting project. It includes creation of .gitignore, main.py, and project-specific settings in multiple .xml files under .idea directory. The WeatherForecaster class is initiated in the main.py file.

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import requests
+
+
+api_key = 'cc90494c608944b39c471433240507m'
+api_url = 'https://www.weatherapi.com/my/'
+
+
+class WeatherForecaster:
+    def __init__(self):
+        pass
